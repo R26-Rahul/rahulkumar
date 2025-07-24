16dabcdf3e084490d9fff4e7abcdc0321868d64e
R26-Rahul <readandwriterai@gmail.com>
2025-07-24 12:14:56 +0530

    form updated

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 
-
-
-
 // ================ mobile devices navbar =======================
-let menuIcon =document.querySelector('#menu-icon');
-let navBar =document.querySelector('.navbar');
-let navid =document.getElementById('navbarid');
+let menuIcon = document.querySelector('#menu-icon');
+let navBar = document.querySelector('.navbar');
+let navid = document.getElementById('navbarid');
 
-menuIcon.onclick =()=>{
+menuIcon.onclick = () => {
   navBar.classList.toggle('active');
-  
-  if(menuIcon.className === 'bx bx-menu'){
-    // menuIcon.classList.toggle('bx-x');
-    menuIcon.className+=' bx-x';
-    navid.style.left='0';
 
-    
-  }else{
-    menuIcon.className ="bx bx-menu";
-    navid.style.left='-100%';
+  if (menuIcon.className === 'bx bx-menu') {
+    // menuIcon.classList.toggle('bx-x');
+    menuIcon.className += ' bx-x';
+    navid.style.left = '0';
+
+
+  } else {
+    menuIcon.className = "bx bx-menu";
+    navid.style.left = '-100%';
   };
-  
+
 }
 
 
@@ -37,14 +34,14 @@ window.onscroll = () => {
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute('id');
-    
+
     if (top >= offset && top < offset + height) {
       navLinks.forEach(links => {
         links.classList.remove('active');
         document.querySelectorAll('header nav a[href*="' + id + '"]').forEach(link => {
           link.classList.add('active');
         });
-        
+
         // document.querySelectorAll('header nav a[href*=' + id + ']').classList.add('active');
       })
     }
@@ -56,7 +53,7 @@ window.onscroll = () => {
   let header = document.querySelector(".header");
   header.classList.toggle('sticky', window.scrollY > 100);
 
-// ================ remove navbar   =======================
+  // ================ remove navbar   =======================
 
   menuIcon.classList.remove('bx-x');
   navBar.classList.remove('active');
@@ -79,9 +76,31 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-let darkmodeIcon= document.querySelector('#darkMoon-icon');
-darkmodeIcon.onclick=()=>{
+let darkmodeIcon = document.querySelector('#darkMoon-icon');
+darkmodeIcon.onclick = () => {
   darkmodeIcon.classList.toggle('bx-sun');
   document.body.classList.toggle('dark-mode')
 
 }
+
+//  google sheet ----------------------------
+const scriptURL = 'https://sheetdb.io/api/v1/5h3m36fryyuoh';
+const form = document.getElementById('formDB');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => Swal.fire({
+      title: "Thank you for contacting us !",
+      text: "You clicked the button!",
+      icon: "success"
+    })).then(()=>{
+      location.reload();
+    })
+    .catch(error => Swal.fire({
+      icon: 'error',
+      title: 'Oops!',
+      text: 'Something went wrong!',
+      confirmButtonText: 'Try Again'
+    }));
+});
